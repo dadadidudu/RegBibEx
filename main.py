25ab41e2def2f961f751f57a419b680ef8857f22
Daniel Heger <da.heger@hotmail.com>
2025-01-17 18:06:55 +0100

    added utf-8 conversion (still has to be tested)

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
+from src.encoding import convert_to_utf8
 from src.extract_journals import ExtractJournals
+import os
 
-file = "./input/ucb_2024.htm"
-ExtractJournals.extract_text(file, "journals", [1, 2], delete_existing=True)
+files = "./input/ucb_2024.htm"
+output_dir = "journals"
+print("extracting")
+ExtractJournals.extract_text(files, output_dir, [1, 2], delete_existing=True)
+
+print("converting to utf-8")
+
+# for root, _, files in os.walk(output_dir):
+#     for f in files:
+#         convert_to_utf8.convert_to_utf8(os.path.join(root, f))
+
 print("done")
