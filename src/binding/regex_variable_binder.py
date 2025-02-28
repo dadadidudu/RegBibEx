73233cc0f4e2b2d5008765b1152aaf0c29de71ec
dadadidudu <dadadidudu@hotmail.com>
2025-02-28 06:33:26 +0100

    error handling, small modifications

--- src/binding/regex_variable_binder.py
+++ src/binding/regex_variable_binder.py
@@ -2,6 +2,7 @@ import re
 from .binder_options import BinderOptions
 
 KEYWORD_VARIABLE_DEFINITION = " as " # {{REGEX as VARIABLE}}
+valid_var_name = re.compile(r"([A-Z][a-z][0-9]_)+")
 
 class RegexVariableBinder:
 	"""
@@ -31,37 +32,44 @@ class RegexVariableBinder:
 		Bindings can either be {{VAR_NAME}} or {{REGEX as VAR_NAME}}.
 		For the former, the regex for VAR_NAME has been defined in the BindingOptions.defaults.
 		The latter will override the default regex with the REGEX value.
+			
 			Parameters:
 				string: the string to apply the regex to
 				regex_with_bindings: the regex containing variable bindings
+			
 			Returns:
 				A dictionary where the requested variables have been bound to their value in the given input string.
 		"""
 		variables_present: list[str] = []
 
 		# convert regex with bindings to a proper regex
-		for var in re.findall(self.variable_finder_pattern, regex_with_bindings):
+		for varDef in re.findall(self.variable_finder_pattern, regex_with_bindings):
 			regex = ""
 			variable = ""
 
-			if (KEYWORD_VARIABLE_DEFINITION in var):
+			if (KEYWORD_VARIABLE_DEFINITION in varDef):
 				# {{REGEX as VARIABLE}}
-				split = var.split(KEYWORD_VARIABLE_DEFINITION, 2)
+				split = varDef.split(KEYWORD_VARIABLE_DEFINITION, 2)
 				regex = split[0]
 				variable = split[1]
 			else:
 				# only {{VARIABLE}}
 				if (self.options is not None):
-					variable = var
-					regex = self.options.defaults[var]
+					variable = varDef
+					regex = self.options.defaults[varDef]
 				else:
-					raise Exception(r"Cannot use RegexVariableBinder with {{VARIABLE}} syntax: Options are missing.")
+					raise Exception(r"Cannot use RegexVariableBinder with {{VARIABLE}} syntax: Defaults are missing.")
 
+			if valid_var_name.match(variable) is None:
+				raise Exception(f"Invalid variable name: {variable}. Variable name must be python compatible.")
+			if regex is None:
+				raise Exception(f"No regex definition for {variable} in defaults.")
+			
 			variables_present.append(variable)
 			new = r"(?P<" + variable + ">" + regex + ")"
-			regex_with_bindings = regex_with_bindings.replace(r"{{" + var + r"}}", new)
+			regex_with_bindings = regex_with_bindings.replace(r"{{" + varDef + r"}}", new)
 		
-		# aapply the converted regex
+		# apply the converted regex
 		compiled_regex = re.compile(regex_with_bindings, re.MULTILINE)
 		r = re.search(compiled_regex, string)
 		if (r is None):
@@ -69,7 +77,8 @@ class RegexVariableBinder:
 		
 		# generate return mapping
 		mappings: dict[str, str] = {}
-		for var in variables_present:
-			var_value = r.group(var)
-			mappings[var] = var_value
+		for varDef in variables_present:
+			var_value = r.group(varDef)
+			mappings[varDef] = var_value
+
 		return mappings
