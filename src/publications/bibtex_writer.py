36ddb8a7eaf0963ac6f566470e7824aa756cb732
dadadidudu <dadadidudu@hotmail.com>
2025-03-03 02:32:40 +0100

    options for entrytypes and citekeys, delete invalind bibtex characters

--- src/publications/bibtex_writer.py
+++ src/publications/bibtex_writer.py
@@ -1,10 +1,17 @@
 import os
+import re
 from ..binding.binder_options import BinderOptions
 from .bibtex import Bibtex
 
+ENTRYTYPE_OPTION_KEY = "entrytype"
+DEFAULT_ENTRYTYPE_OPTION = "article"
+CITEKEY_OPTION_KEY = "citekey"
+DEFAULT_CITEKEY_OPTION = "title"
+MAX_CITEKEY_LENGTH = 25
 
 class BibtexWriter:
 	
+	__i = 0
 	__directory: str
 	__options: BinderOptions
 
@@ -19,19 +26,37 @@ class BibtexWriter:
 			content += "\n"
 		return content
 	
-	def __get_entrytype(self) -> str:
-		return "article"
+	def __get_entrytype(self, filename: str) -> str:
+		default_entrytype = self.__options.defaults.get(ENTRYTYPE_OPTION_KEY, DEFAULT_ENTRYTYPE_OPTION)
+		entrytype_for_this_file = self.__options.get_individual_options(filename).get(ENTRYTYPE_OPTION_KEY, default_entrytype)
+		return entrytype_for_this_file
 
-	def __get_citekey(self) -> str:
-		return "cite1"
+	def __get_citekey(self, filename: str, bibtex: Bibtex) -> str:
+		default_citekey = self.__options.defaults.get(CITEKEY_OPTION_KEY, DEFAULT_CITEKEY_OPTION)
+		citekey_for_this_file = self.__options.get_individual_options(filename).get(CITEKEY_OPTION_KEY, default_citekey)
+		
+		citekey_mapping = bibtex.get_field_value(citekey_for_this_file)
+		
+		if (citekey_mapping == ""):
+			self.__i += 1
+			return f"{DEFAULT_CITEKEY_OPTION}-{self.__i}"
+		else:
+			ignorechars = r"[^\w\d]"
+			citekey_mapping = re.sub(ignorechars, "", citekey_mapping)
+			citekey_mapping = citekey_mapping.replace(" ", "-")
+			if (len(citekey_mapping) > MAX_CITEKEY_LENGTH):
+				return citekey_mapping[0:MAX_CITEKEY_LENGTH]
+			else:
+				return citekey_mapping
 
 	def write_bibtex_to_file(self, filename: str, file_content: list[Bibtex]):
 		if (os.path.isdir(self.__directory) is False):
 			os.makedirs(self.__directory)
 
-		entrytype = self.__get_entrytype()
-		citekey = self.__get_citekey()
+		entrytype = self.__get_entrytype(filename)
+		
 
 		with open(f"{self.__directory}/{filename}.bibtex", mode="w") as f:
 			for btx in file_content:
+				citekey = self.__get_citekey(filename, btx)
 				f.write(f"""@{entrytype}{{{citekey},{self.__get_bibtex_content(btx)}}}\n""")
